b6d57df38dd44fe633a156244ddf61615cca8afb
Rishabh Saxena <orishabh1@gmail.com>
2016-05-11 14:45:20 +0530

    temp

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,3 +39,8 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
+
+config.after_initialize do
+  ActiveMerchant::Billing::Base.mode = :test
+
+end
