cf908228fab97a12f96d0aaf55a58454a570be29
Rishabh Saxena <orishabh1@gmail.com>
2016-05-12 17:18:39 +0530

    form left

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,14 @@
+class WelcomeController < ApplicationController
+  def index
+
+  end
+
+  def create
+    @subscription = Subscription.new(params[:subscription])
+    if @subscription.save_with_payment
+      redirect_to @subscription, :notice => "Thank you for subscribing!"
+    else
+     render :new
+    end
+  end
+end
