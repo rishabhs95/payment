7a6e71c56c49dbeedec7712b3b744248013b9710
Rishabh Saxena <orishabh1@gmail.com>
2016-05-12 14:23:55 +0530

    temp

---
+++ config/initializers/stripe.rb
@@ -0,0 +1,2 @@
+Stripe.api_key = "sk_test_wf7bgtFuXopAiLQP33BQKsqG"
+STRIPE_PUBLIC_KEY = "pk_test_QuZ0gtLfMTZmuWjzLCEXiYdM"
---
+++ db/migrate/20160512052835_create_subscriptions.rb
@@ -0,0 +1,6 @@
+class CreateSubscriptions < ActiveRecord::Migration
+  def change
+    create_table :subscriptions do |t|
+    end
+  end
+end
